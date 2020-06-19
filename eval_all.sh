bc5f754a46512605da93a96319a6eb1d778380f2
danifos <danifosgm@gmail.com>
2020-06-19 20:26:58 +0800

    Clear legacy

--- eval_all.sh
+++
@@ -1,5 +0,0 @@
-for dir in results/*
-do
-    echo $dir
-    python eval_acc.py -i $dir/test_latest/images/*_fake_B.png -l ../dataset/new_test_set/test_label/*
-done
--- pytorch-U-Net/eval_all.sh
+++ pytorch-U-Net/eval_all.sh
@@ -1,5 +1,5 @@
 for dir in *U-net*
 do
     echo $dir
-    python ../eval_acc.py -i $dir/eval/class_1_image_*.tif -l ../../dataset/new_test_set/test_label/*
+    python ../scripts/eval_acc.py -i $dir/eval/class_1_image_*.tif -l ../dataset/new_test_set/test_label/*
 done
